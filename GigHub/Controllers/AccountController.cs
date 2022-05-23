f6385526bf18adc6cb0e7436a247a484f0fe0bf6
Brian Yap <brian3190@gmail.com>
2022-05-23 14:36:13 +0800

    Add project files.

---
+++ GigHub/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Models;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager userManager;
+        
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false)
+                }
+            }
+        }
+    }
+}
---
+++ GigHub/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace GigHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser>)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie)
+            return userIdentity;
+        }
+    } 
+}
